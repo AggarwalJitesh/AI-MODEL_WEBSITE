0b8b6baa2f2f9d93eda5caf36415b74bf296ab9a
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2023-07-24 20:59:03 +0530

    Add files via upload

---
+++ src/components/pages/dashboard.js
@@ -0,0 +1,18 @@
+import Rechart from "../inc/rechart";
+import { Link } from "react-router-dom";
+import Navigation from "../inc/navigation";
+
+function Dashboard() {
+  return (
+    <>
+      <div class="banner1">
+        <Navigation />
+        <div class="d-flex justify-content-center">
+          <Rechart />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
